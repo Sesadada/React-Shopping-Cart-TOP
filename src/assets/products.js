c907f8da450108b5f1ec0253661f1daf9c61353f
Sesadada <serenaolivieri@riseup.net>
2021-05-21 20:11:11 +0200

    last changes

--- src/assets/products.js
+++ src/assets/products.js
@@ -2,7 +2,7 @@ const products = [
 	{
 		_id: '1',
 		name: 'Black ink Witch Craft Tea Towel',
-		image: '/images/IMG_1.JPEG',
+		image: './images/IMG_1.JPEG',
 		description:
 			'Beige Tea Towel, witchcrafted in Italy, screenprinted in Spain with pink and black ink',
 		category: 'tea towel',
@@ -12,7 +12,7 @@ const products = [
 	{
 		_id: '2',
 		name: 'Black ink Witch Craft Tea Towel',
-		image: '/images/IMG_2.JPEG',
+		image: './images/IMG_2.JPEG',
 		description:
 			'Beige Tea Towel, witchcrafted in Italy, screenprinted in Spain with black ink',
 		category: 'tea towel',
@@ -22,7 +22,7 @@ const products = [
 	{
 		_id: '3',
 		name: 'Pink ink Witch Craft Tea Towel',
-		image: '/images/IMG_3.JPEG',
+		image: './images/IMG_3.JPEG',
 		description:
 			'Beige Tea Towel, witchcrafted in Italy, screenprinted in Spain with pink ink',
 		category: 'tea towel',
@@ -32,7 +32,7 @@ const products = [
 	{
 		_id: '4',
 		name: 'Black Witch Craft Pillow',
-		image: '/images/IMG_4.JPEG',
+		image: './images/IMG_4.JPEG',
 		description:
 			'Beige Pillow, witchcrafted in Italy, screenprinted in Spain with black ink',
 		category: 'pillow',
@@ -42,7 +42,7 @@ const products = [
 	{
 		_id: '5',
 		name: 'Beige Witch Craft Tote Bag',
-		image: '/images/IMG_5.JPEG',
+		image: './images/IMG_5.JPEG',
 		description:
 			'Beige Tote Bag, witchcrafted in Italy, screenprinted in Spain with pink ink.The drawing has a wolf and a woman, from ancient legends of central Italy',
 		category: 'tote bag',
@@ -52,7 +52,7 @@ const products = [
 	{
 		_id: '6',
 		name: 'Beige Witch Craft Tea Towel',
-		image: '/images/IMG_6.JPEG',
+		image: './images/IMG_6.JPEG',
 		description:
 			'Beige Tea Towel, witchcrafted in Italy, screenprinted in Spain with pink ink, representing the ensemble of legends of central Italy involving the bad eye',
 		category: 'tote bag',
@@ -62,7 +62,7 @@ const products = [
 	{
 		_id: '7',
 		name: 'Pink Witch Craft Tea Towel',
-		image: '/images/IMG_7.JPEG',
+		image: './images/IMG_7.JPEG',
 		description:
 			'Pink Tea Towel, witchcrafted in Italy, screenprinted in Spain with pink ink, representing the ensemble of legends of central Italy involving the bad eye',
 		category: 'tote bag',
@@ -72,7 +72,7 @@ const products = [
 	{
 		_id: '8',
 		name: 'Grey Witch Craft Tea Towel',
-		image: '/images/IMG_8.JPEG',
+		image: './images/IMG_8.JPEG',
 		description:
 			'Grey Tea Towel, witchcrafted in Italy, screenprinted in Spain with pink ink, representing the ensemble of legends of central Italy involving the bad eye',
 		category: 'tote bag',
@@ -82,7 +82,7 @@ const products = [
 	{
 		_id: '9',
 		name: 'Vinaccio Witch Craft Tea Towel',
-		image: '/images/IMG_9.JPEG',
+		image: './images/IMG_9.JPEG',
 		description:
 			'Vinaccio Tea Towel, witchcrafted in Italy, screenprinted in Spain with black ink, representing the ensemble of legends of central Italy involving the bad eye',
 		category: 'tote bag',
